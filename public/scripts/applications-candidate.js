776481facfdeca0e977331037c453b12b8e85382
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-05-12 18:07:06 +0100

    title used from the position

--- public/scripts/applications-candidate.js
+++ public/scripts/applications-candidate.js
@@ -6,10 +6,12 @@
             application.is_accepted = application.candidate_status === "accepted";
 
             application.date_applied = moment(application.date_applied).fromNow();
-
-            $("#applications-container").append(
-                Utils.fillTemplate("application-template", application)
-            );
+            $.get("/api/positions/" + application.job_position_id, function(data) {
+                application.title = data.title;
+                $("#applications-container").append(
+                    Utils.fillTemplate("application-template", application)
+                );
+            });
         });
 
         $(".application-withdraw-button").click(actions.withdrawApplication);
