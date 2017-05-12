e2f29f5070a645b8885072e464cc566081fcf065
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-05-12 17:49:41 +0100

    actions set globally in a more JS way

--- public/scripts/positionDeletion.js
+++ public/scripts/positionDeletion.js
@@ -1,16 +1,17 @@
-let actions = {
-    deletePosition: function (e) {
-        e.preventDefault();
-        if (!confirm("Are you sure you want to delete this position?")) {
-            return;
-        }
-
-        $.ajax({
-            method: "DELETE",
-            url: this.href
-        })
-        .done(function() {
-            window.location = "./job-positions.html";
-        });
+if (window.actions == undefined) {
+    actions = {};
+}
+actions.deletePosition = function (e) {
+    e.preventDefault();
+    if (!confirm("Are you sure you want to delete this position?")) {
+        return;
     }
+
+    $.ajax({
+        method: "DELETE",
+        url: this.href
+    })
+    .done(function() {
+        window.location = "./job-positions.html";
+    });
 };
