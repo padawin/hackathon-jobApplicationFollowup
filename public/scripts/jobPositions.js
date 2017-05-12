4799165a2fa82648ee64446e513cbdec5c8d272c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-05-12 11:34:35 +0100

    dates displayed properly

--- public/scripts/jobPositions.js
+++ public/scripts/jobPositions.js
@@ -1,6 +1,7 @@
 (function () {
     $.get("/api/positions", function(data) {
         data.forEach(function(position) {
+            position.date_posted = moment(position.date_posted).fromNow();
             $("#positions-container").append(
                 Utils.fillTemplate("position-template", position)
             );
