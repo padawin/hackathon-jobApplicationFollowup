a240e422366fb093230ea7caff3a74ceabd546d6
Robert Wright <madman.bob@hotmail.co.uk>
2017-05-12 14:18:07 +0100

    Added date created to Application object

--- applications.py
+++ applications.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+
 class Application:
-    def __init__(self, application_id, job_position_id, candidate_name, candidate_status):
+    def __init__(self, application_id, job_position_id, candidate_name, candidate_status, date_applied=None):
         self.application_id = application_id
         self.job_position_id = job_position_id
         self.candidate_name = candidate_name
         self.candidate_status = candidate_status
+        self.date_applied = date_applied or datetime.now()
