2d9482c4ff1f9b8490a61023442adfe0386cb167
Lewis Putz <lewis@workdigital.co.uk>
2017-05-12 13:26:15 +0100

    job position form posting

---
+++ public/scripts/jobPositionForm.js
@@ -0,0 +1,37 @@
+$('#position_form').submit(function(event) {
+    event.preventDefault();
+
+    var title = $('#jobTitle').val().trim();
+    var description = $('#jobDescription').val().trim();
+
+    // simple validation
+    if (!title) {
+        $('#jobTitle').parent().addClass('has-danger');
+    }
+
+    if (!description) {
+        $('#jobDescription').parent().addClass('has-danger');
+    }
+
+    if (!title || !description) {
+        return;
+    }
+
+    var formData = {
+        job_position_id: 0,
+        title: title,
+        description: description,
+        date_posted: (new Date()).toISOString()
+    };
+
+    $.ajax({
+            url: '/api/positions',
+            type: 'POST',
+            data: JSON.stringify(formData),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (response) {
+                window.location.href = '/job-position-admin.html?id=' + response.job_position_id
+            }
+    });
+});
