e4eeaf26d578189b2da683ad8c1adb284150262c
Lewis Putz <lewis@workdigital.co.uk>
2017-05-12 16:28:22 +0100

    Fix images by using index+1 for image ID

--- public/scripts/jobPositions.js
+++ public/scripts/jobPositions.js
@@ -1,6 +1,7 @@
 (function () {
     $.get("/api/positions", function(data) {
         data.forEach(function(position) {
+            position.image_id = (position.job_position_id + 1);
             position.date_posted = moment(position.date_posted).fromNow();
             $("#positions-container").append(
                 Utils.fillTemplate("position-template", position)
