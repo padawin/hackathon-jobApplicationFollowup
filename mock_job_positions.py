34262d2e9623df195a74352027c6ae8fdf9bb655
Robert Wright <madman.bob@hotmail.co.uk>
2017-05-12 16:37:27 +0100

    Sort job positions list

--- mock_job_positions.py
+++ mock_job_positions.py
@@ -22,16 +22,7 @@ _mock_job_positions = [
         nibh auctor, porttitor ex ac, accumsan leo. Quisque accumsan ultricies sem in posuere. Cras
         ultricies, felis ac accumsan fermentum, nulla tellus egestas nisi, et venenatis arcu elit a velit.
         Fusce dictum faucibus nisl quis iaculis. Aliquam maximus pulvinar est.
-    """, datetime(2017, 5, 12), is_new=True),
-    JobPosition(next(mock_ids), "Software Developer", """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nisi erat, lobortis sed purus id,
-        vulputate iaculis turpis. Curabitur vehicula ornare felis dapibus ultricies. Sed pretium augue et
-        libero consectetur, vel sagittis turpis bibendum. Duis semper porta lectus quis convallis. Nulla
-        feugiat rhoncus convallis. Nam elementum sem in sem tincidunt, nec tristique eros fringilla. Ut nec
-        nibh auctor, porttitor ex ac, accumsan leo. Quisque accumsan ultricies sem in posuere. Cras
-        ultricies, felis ac accumsan fermentum, nulla tellus egestas nisi, et venenatis arcu elit a velit.
-        Fusce dictum faucibus nisl quis iaculis. Aliquam maximus pulvinar est.
-    """, datetime(2017, 5, 10)),
+    """, datetime(2017, 5, 4)),
     JobPosition(next(mock_ids), "Software Developer", """
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nisi erat, lobortis sed purus id,
         vulputate iaculis turpis. Curabitur vehicula ornare felis dapibus ultricies. Sed pretium augue et
@@ -49,7 +40,16 @@ _mock_job_positions = [
         nibh auctor, porttitor ex ac, accumsan leo. Quisque accumsan ultricies sem in posuere. Cras
         ultricies, felis ac accumsan fermentum, nulla tellus egestas nisi, et venenatis arcu elit a velit.
         Fusce dictum faucibus nisl quis iaculis. Aliquam maximus pulvinar est.
-    """, datetime(2017, 5, 4))
+    """, datetime(2017, 5, 10)),
+    JobPosition(next(mock_ids), "Software Developer", """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nisi erat, lobortis sed purus id,
+        vulputate iaculis turpis. Curabitur vehicula ornare felis dapibus ultricies. Sed pretium augue et
+        libero consectetur, vel sagittis turpis bibendum. Duis semper porta lectus quis convallis. Nulla
+        feugiat rhoncus convallis. Nam elementum sem in sem tincidunt, nec tristique eros fringilla. Ut nec
+        nibh auctor, porttitor ex ac, accumsan leo. Quisque accumsan ultricies sem in posuere. Cras
+        ultricies, felis ac accumsan fermentum, nulla tellus egestas nisi, et venenatis arcu elit a velit.
+        Fusce dictum faucibus nisl quis iaculis. Aliquam maximus pulvinar est.
+    """, datetime(2017, 5, 12), is_new=True)
 ]
 
 mock_job_positions = {
@@ -60,7 +60,7 @@ mock_job_positions = {
 class MockJobPositionsList(Resource):
     @staticmethod
     def get():
-        return list(mock_job_positions.values())
+        return sorted(mock_job_positions.values(), key=lambda position: position.job_position_id, reverse=True)
 
     @staticmethod
     def post():
