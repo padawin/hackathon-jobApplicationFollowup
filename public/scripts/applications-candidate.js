e98c50074fc874eadf09456aff7e990502afefb3
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-05-12 17:14:09 +0100

    application list for a candidate is now dynamic

---
+++ public/scripts/applications-candidate.js
@@ -0,0 +1,15 @@
+(function () {
+    $.get("/api/applications", function(data) {
+        data.forEach(function(application) {
+            application.is_pending = application.candidate_status === "pending";
+            application.is_rejected = application.candidate_status === "rejected";
+            application.is_accepted = application.candidate_status === "accepted";
+
+            application.date_applied = moment(application.date_applied).fromNow();
+
+            $("#applications-container").append(
+                Utils.fillTemplate("application-template", application)
+            );
+        });
+    });
+})();
