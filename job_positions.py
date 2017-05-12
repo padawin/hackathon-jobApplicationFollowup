4cf769926d1d16b87b1ae044ac9456961c11cf08
Robert Wright <madman.bob@hotmail.co.uk>
2017-05-12 14:17:41 +0100

    Made JobPosition date default to now

--- job_positions.py
+++ job_positions.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
+
 class JobPosition:
-    def __init__(self, job_position_id, title, description, date_posted, is_new=False):
+    def __init__(self, job_position_id, title, description, date_posted=None, is_new=False):
         self.job_position_id = job_position_id
         self.title = title
         self.description = description
-        self.date_posted = date_posted
+        self.date_posted = date_posted or datetime.now()
         self.is_new = is_new
